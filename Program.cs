c6f4cac2b7ca03d5808011a9aff58d0531b07125
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-01 13:09:50 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LineComparisonProblems  // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line Comparison Computation program");
+            LineComparisonProblem.Sample.CaluculateRoot();
+            LineComparisonProblem.CheckQuality.EqualLines();
+            LineComparisonProblem.GreaterOrLess.CompareLine();
+
+        }
+    }
+}
