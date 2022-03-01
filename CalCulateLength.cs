76e47935a9ee10d3f65bd517ce85af76db8f8c82
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-01 13:13:43 +0530

    CalCulateLength.cs push for LineComarison_uc1

---
+++ CalCulateLength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+	public class Sample
+	{
+
+		public static void CaluculateRoot()
+		{
+
+			// point 1
+			double x1 =1;
+			double y1 =2;
+
+			// point 2
+			double x2=5;
+			double y2=5;
+
+			// Calculate distance between two points
+			double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)
+					* (y2 - y1));
+
+			Console.WriteLine("Distance between two points :" + distance);
+		}
+
+	}
+}
